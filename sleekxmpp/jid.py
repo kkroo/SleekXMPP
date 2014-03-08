25dcbd9f6e54e2b8bc986e1528956535f7292975
Omar Ramadan <omar@fusi.io>
2014-03-07 18:49:22 -0800

    removed the restriction on the @ in the user field of the JID for compatibility with FB non-standard JID form

--- sleekxmpp/jid.py
+++ sleekxmpp/jid.py
@@ -28,13 +28,13 @@ from sleekxmpp.thirdparty import OrderedDict
 ILLEGAL_CHARS = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r' + \
                 '\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19' + \
                 '\x1a\x1b\x1c\x1d\x1e\x1f' + \
-                ' !"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~\x7f'
+                ' !"#$%&\'()*+,./:;<=>?[\\]^_`{|}~\x7f'
 
 #: The basic regex pattern that a JID must match in order to determine
 #: the local, domain, and resource parts. This regex does NOT do any
 #: validation, which requires application of nodeprep, resourceprep, etc.
 JID_PATTERN = re.compile(
-    "^(?:([^\"&'/:<>@]{1,1023})@)?([^/@]{1,1023})(?:/(.{1,1023}))?$"
+    "^(?:([^\"&'/:<>]{1,1023})@)?([^/@]{1,1023})(?:/(.{1,1023}))?$"
 )
 
 #: The set of escape sequences for the characters not allowed by nodeprep.
@@ -107,7 +107,7 @@ nodeprep = stringprep_profiles.create(
         stringprep.in_table_c7,
         stringprep.in_table_c8,
         stringprep.in_table_c9,
-        lambda c: c in ' \'"&/:<>@'],
+        lambda c: c in ' \'"&/:<>'],
     unassigned=[stringprep.in_table_a1])
 
 # pylint: disable=c0103
